a92ca1970f67aedc3c31b334883c32587bc1f7e7
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-10-06 16:15:52 +0200

    Finished adding loggs and publich to azure portal by using serilog

--- CosmosWebApp/Pages/ClothesOverview.cshtml.cs
+++ CosmosWebApp/Pages/ClothesOverview.cshtml.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using CosmosWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace CosmosWebApp.Pages
 {
     public class ClothesOverviewModel : PageModel
     {
         private readonly ICosmosDbService _cosmosDbService;
-        public ClothesOverviewModel(ICosmosDbService cosmosDbService)
+        private readonly ILogger<ClothesOverviewModel> _logger;
+        public ClothesOverviewModel(ICosmosDbService cosmosDbService, ILogger<ClothesOverviewModel> logger)
         {
             _cosmosDbService = cosmosDbService;
+            _logger = logger;
         }
 
         [BindProperty]
@@ -24,12 +27,18 @@ namespace CosmosWebApp.Pages
         public async Task<IActionResult> OnGet()
         {
             var result = await _cosmosDbService.GetItemsAsync("SELECT * FROM c");
-
-            foreach (var clothItem in result)
+            if (result == null)
             {
-                ClothesList.Add(clothItem);
+                _logger.LogWarning("No items has been found!");
             }
-
+            else
+            {
+                foreach (var clothItem in result)
+                {
+                    ClothesList.Add(clothItem);
+                }
+                _logger.LogInformation("Items has been added");
+            }
             return Page();
         }
     }
--- CosmosWebApp/Program.cs
+++ CosmosWebApp/Program.cs
@@ -2,22 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace CosmosWebApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.ApplicationInsights(new TelemetryConfiguration { InstrumentationKey = "529e408c-cf60-4437-8150-3e3b32991f00" }, TelemetryConverter.Traces)
+            .CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
