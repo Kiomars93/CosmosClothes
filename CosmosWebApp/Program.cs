49e3b715eb78746e4f079fd6ae5911e27eb1f6eb
Kio <48633146+Kiomars93@users.noreply.github.com>
2021-10-06 16:45:36 +0200

    Finally added serlog

--- CosmosWebApp/Program.cs
+++ CosmosWebApp/Program.cs
@@ -7,18 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-<<<<<<< HEAD
 using Serilog;
 using Serilog.Events;
-=======
-<<<<<<< HEAD
-using Serilog;
-using Serilog.Events;
-=======
-using NLog;
-using NLog.Web;
->>>>>>> 182fb193b8b924df1e233daf32963c43a39edd29
->>>>>>> b78c9acfaba09e110a98544cd56a6599859d6b84
 
 namespace CosmosWebApp
 {
@@ -26,13 +16,8 @@ namespace CosmosWebApp
     {
         public static int Main(string[] args)
         {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b78c9acfaba09e110a98544cd56a6599859d6b84
             Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
             .Enrich.FromLogContext()
             .WriteTo.Console()
             .WriteTo.ApplicationInsights(new TelemetryConfiguration { InstrumentationKey = "529e408c-cf60-4437-8150-3e3b32991f00" }, TelemetryConverter.Traces)
@@ -51,41 +36,14 @@ namespace CosmosWebApp
             finally
             {
                 Log.CloseAndFlush();
-<<<<<<< HEAD
-=======
-=======
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                logger.Debug("init main");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception e)
-            {
-                logger.Error(e, "Stopped program because of exception");
-                throw;
-            }
-            finally
-            {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                LogManager.Shutdown();
->>>>>>> 182fb193b8b924df1e233daf32963c43a39edd29
->>>>>>> b78c9acfaba09e110a98544cd56a6599859d6b84
             }
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
             .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                })
-            .ConfigureLogging(logging => 
-            {
-                logging.ClearProviders();
-                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-            })
-            .UseNLog(); // NLog: Setup NLog for Dependency injection
+                });
+        }
     }
-}
